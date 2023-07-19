3692412271c4fc8877bc8642fb496cbd038da8f0
alok singh thakur <alokthakur08cse@gmail.com>
2023-07-19 17:56:54 +0530

    Performed CRUD operation in Business Contact

--- src/App.java
+++ src/App.java
@@ -1,133 +1,169 @@
-const api = require("./famark-cloud");
-const promptSync = require("prompt-sync");
-const prompt = promptSync();
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Scanner;
 
-async function main() {
-    //Assigning Credentials
-    const domainName = prompt("Enter Domain Name: ");
-    const userName = prompt("Enter UserName: ");
-    const password = prompt("Enter Password: ");
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
 
-    const sessionId = await getSessionId(domainName, userName, password);
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
-    if (sessionId == null) {
-        console.log("Login Failed!");
-        return;
-    }
+public class App {
 
-    let ans = prompt("Do you want to create a new record? (Y or N): ");
-    while (ans == "Y" || ans == "y") {
-        const firstName = prompt("Enter First Name: ");
-        const lastName = prompt("Enter Last Name: ");
-        const phone = prompt("Enter Phone: ");
-        const email = prompt("Enter Email: ")
-
-        const contactRecord = {
-            FirstName: firstName,
-            LastName: lastName,
-            Phone: phone,
-            Email: email
-        };
-
-        const businessContactId = await api.postData(
-            "/Business_Contact/CreateRecord",
-            JSON.stringify(contactRecord),
-            sessionId
-        );
-
-        console.log("Created RecordId: " + businessContactId);
-        ans = prompt("Do you want to create a new record? (Y or N): ");
-    }
-
-    //Retrieving Business_Contact records
-    const retrieveMultipleQuery = {
-        Columns: "FullName, Phone, Email, Business_ContactId",
-        OrderBy: "FullName"
-    };
-
-    const businessContacts = await api.postData(
-        "/Business_Contact/RetrieveMultipleRecords",
-        JSON.stringify(retrieveMultipleQuery),
-        sessionId
-    );
-
-    for (let i = 0; i < businessContacts.length; i++) {
-        console.log(businessContacts[i]);
-    }
-
-    //Update Contact
-    ans = prompt("Do you want to update a record? (Y or N): ");
-    if (ans == "Y" || ans == "y") {
-
-        const contactId = prompt("Enter ContactId: ");
-        const firstName = prompt("Enter First Name: ");
-        const lastName = prompt("Enter Last Name: ");
-        const phone = prompt("Enter Phone: ");
-        const email = prompt("Enter Email: ")
-
-        const contactRecord = {
-            FirstName: firstName,
-            LastName: lastName,
-            Phone: phone,
-            Email: email,
-            Business_ContactId: contactId
-        };
-
-        const businessContactId = await api.postData(
-            "/Business_Contact/UpdateRecord",
-            JSON.stringify(contactRecord),
-            sessionId
-        );
-
-        //Retrieve Single Record (Updated Record by Id)
-        if (businessContactId != null) {
-            const retrieveRecordQuery = {
-                Columns: "FullName, Phone, Email, Business_ContactId",
-                Business_ContactId: contactId
-            };
-
-            const updatedContact = await api.postData(
-                "/Business_Contact/RetrieveRecord",
-                JSON.stringify(retrieveRecordQuery),
-                sessionId
-            );
-
-            console.log("Updated Record:-");
-            console.log(updatedContact);
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Domain Name: ");
+        String domainName = input.next();
+        System.out.println("Enter User Name: ");
+        String userName = input.next();
+        System.out.println("Enter Password: ");
+        String password = input.next();
+        HashMap<String, Object> credData = new HashMap<String, Object>();
+        credData.put("DomainName", domainName);
+        credData.put("UserName", userName);
+        credData.put("Password", password);
+        JSONObject credDataJson = new JSONObject(credData);
+        String request = credDataJson.toJSONString();
+        Reader reader = postData("/Credential/Connect", request, null);
+        Object obj = JSONValue.parse(reader);
+        String sessionID = (String) obj;
+        System.out.println(sessionID);
+        if (sessionID == null || sessionID.length() == 0) {
+            input.close();
+            return;
         }
+
+        // Creating records by taking input from user
+        System.out.println("Do you want to create a new record (y/n): ");
+        char userResponse = input.next().charAt(0);
+        while (userResponse == 'Y' || userResponse == 'y') {
+            // Clearing new line from buffer
+            input.nextLine();
+            System.out.println("Enter FirstName: ");
+            String firstName = input.nextLine();
+            System.out.println("Enter LastName: ");
+            String lastName = input.nextLine();
+            System.out.println("Enter Phone: ");
+            String phone = input.nextLine();
+            System.out.println("Enter Email: ");
+            String email = input.nextLine();
+            HashMap<String, Object> contactData = new HashMap<String, Object>();
+            contactData.put("FirstName", firstName);
+            contactData.put("LastName", lastName);
+            contactData.put("Phone", phone);
+            contactData.put("Email", email);
+            JSONObject contactDataJson = new JSONObject(contactData);
+            request = contactDataJson.toJSONString();
+            reader = postData("/Business_Contact/CreateRecord", request, sessionID);
+            obj = JSONValue.parse(reader);
+            String recordID = (String) obj;
+            if (recordID != null && recordID.length() > 0) {
+                System.out.println("Created RecordId: \n" + recordID);
+                System.out.println("Do you want to create more System record (y/n)? ");
+            } else {
+                System.out.println("Do you want to try again (y/n)? ");
+            }
+            userResponse = input.next().charAt(0);
+        }
+        // input.close();
+
+        // Calling RetrieveMultipleRecords action on the System_Profile entity
+        HashMap<String, Object> retrieveData = new HashMap<String, Object>();
+        retrieveData.put("Columns", "FirstName,LastName,Phone,Email,Business_ContactId");
+        retrieveData.put("OrderBy", "FirstName");
+        JSONObject retrieveDataJson = new JSONObject(retrieveData);
+        request = retrieveDataJson.toJSONString();
+        reader = postData("/Business_Contact/RetrieveMultipleRecords", request, sessionID);
+        JSONParser parser = new JSONParser();
+        JSONArray array = (JSONArray) parser.parse(reader);
+
+        // Checking whether the JSON array has some value or not
+        if (array != null) {
+
+            // Iterating JSON array
+            System.out.println("Records are: ");
+            for (int i = 0; i < array.size(); i++) {
+                System.out.println(i + 1 + ": " + array.get(i));
+            }
+        }
+        
+        // Deleting records by taking input from user
+        System.out.println("Do you want delete a record (y/n): ");
+        char userDeleteResponse = input.next().charAt(0);
+        while (userDeleteResponse == 'Y' || userDeleteResponse == 'y') {
+            input.nextLine();
+            System.out.println("Enter ContactId: ");
+            String contactId = input.nextLine();
+            HashMap<String, Object> businessData = new HashMap<String, Object>();
+            businessData.put("Business_ContactId", contactId);
+            JSONObject businessDataJson = new JSONObject(businessData);
+            request = businessDataJson.toJSONString();
+            postData("/Business_Contact/DeleteRecord", request, sessionID);
+            System.out.println("Record Deleted");
+            System.out.println("Do you want delete a record (y/n): ");
+            userDeleteResponse = input.next().charAt(0);
+        }
+
+        // Updating records by taking user from user
+        System.out.println("Do you want update a record (y/n): ");
+        char userUpdateResponse = input.next().charAt(0);
+          while (userUpdateResponse == 'Y' || userUpdateResponse == 'y') {
+            // Clearing new line from buffer
+            input.nextLine();
+            System.out.println("Enter ContactId: ");
+            String contactId = input.nextLine();
+            System.out.println("Enter FirstName: ");
+            String firstName = input.nextLine();
+            System.out.println("Enter LastName: ");
+            String lastName = input.nextLine();
+            System.out.println("Enter Phone: ");
+            String phone = input.nextLine();
+            System.out.println("Enter Email: ");
+            String email = input.nextLine();
+            HashMap<String, Object> updateData = new HashMap<String, Object>();
+            updateData.put("Business_ContactId", contactId);
+            updateData.put("FirstName", firstName);
+            updateData.put("LastName", lastName);
+            updateData.put("Phone", phone);
+            updateData.put("Email", email);
+            JSONObject updateDataJson = new JSONObject(updateData);
+            request = updateDataJson.toJSONString();
+            postData("/Business_Contact/UpdateRecord", request, sessionID);
+            System.out.println("Record Updated");
+            System.out.println("Do you want update a record (y/n): ");
+            userUpdateResponse = input.next().charAt(0);
+        }
+        input.close();
     }
 
-    ans = prompt("Do you want to delete a record? (Y or N): ");
-    if (ans == "Y" || ans == "y") {
-
-        const contactId = prompt("Enter ContactId: ");
-        const deleteRecordDate = {
-            Business_ContactId: contactId
-        };
-
-        await api.postData(
-            "/Business_Contact/DeleteRecord",
-            JSON.stringify(deleteRecordDate),
-            sessionId
-        );
-
-        console.log("Record Deleted!");
+    private static Reader postData(String urlSuffix, String body, String sessionID) throws IOException {
+        URL url = new URL("https://www.famark.com/host/api.svc/api" + urlSuffix);
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) con;
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        byte[] out = body.getBytes(StandardCharsets.UTF_8);
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.setRequestProperty("Content-Length", Integer.toString(out.length));
+        if (sessionID != null && sessionID.length() > 0) {
+            http.setRequestProperty("SessionId", sessionID);
+        }
+        try (DataOutputStream os = new DataOutputStream(http.getOutputStream())) {
+            os.write(out);
+        }
+        String errorMessage = http.getHeaderField("ErrorMessage");
+        if (errorMessage != null && errorMessage.length() > 0) {
+            System.err.println(errorMessage);
+            return null;
+        }
+        return new BufferedReader(new InputStreamReader(http.getInputStream(), "UTF-8"));
     }
 }
-
-async function getSessionId(domainName, userName, password) {
-    const credential = {
-        DomainName: domainName,
-        UserName: userName,
-        Password: password,
-    };
-
-    return await api.postData(
-        "/Credential/Connect",
-        JSON.stringify(credential),
-        null
-    );
-}
-
-//calling main method
-main();
